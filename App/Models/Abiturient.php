268e3e0ef4e1c5013ccf8fb5d8312f71cd80aa99
theifel <condecrom@gmail.com>
2021-02-12 14:16:38 +0300

    Added the abiturient entity

---
+++ App/Models/Abiturient.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Models;
+
+
+class Abiturient
+{
+
+    public int $id;
+    public string $firstName;
+    public string $lastName;
+    public string $gender;
+    public int $groupNumber;
+    public string $email;
+    public int $score;
+    public int $birthYear;
+    public string $city;
+}
