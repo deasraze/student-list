a4c634e6bec26c921eedd865dc9bc6966c104385
theifel <condecrom@gmail.com>
2021-02-14 13:15:24 +0300

    Fixed of the registration/editorial form

--- App/Views/register.php
+++ App/Views/register.php
@@ -25,16 +25,8 @@
                 <input type="text" id="inputLastName" class="form-control" required>
             </div>
             <div class="col-md-6">
-                <label for="inputEmail" class="form-label">Email</label>
-                <input type="email" id="inputEmail" class="form-control" placeholder="example@gmail.com" required>
-            </div>
-            <div class="col-md-6">
-                <label for="inputGroupNumber" class="form-label">Group number</label>
-                <input type="text" id="inputGroupNumber" class="form-control" required>
-            </div>
-            <div class="col-md-6">
-                <label for="inputScore" class="form-label">Score</label>
-                <input type="number" min="0" max="1100" id="inputScore" class="form-control" placeholder="Total points for the Unified State Exam" required>
+                <label for="inputBirthYear" class="form-label">Birth year</label>
+                <input type="number" min="1900" id="inputLastName" class="form-control" required>
             </div>
             <div class="col-md-6">
                 <label for="inputGender" class="form-label">Gender</label>
@@ -43,6 +35,26 @@
                     <option>Male</option>
                     <option>Female</option>
                 </select>
+            </div>
+            <div class="col-md-6">
+                <label for="inputGroupNumber" class="form-label">Group number</label>
+                <input type="text" id="inputGroupNumber" class="form-control" placeholder="Example: 1010Э, 132М, 00123" required>
+            </div>
+            <div class="col-md-6">
+                <label for="inputScore" class="form-label">Score</label>
+                <input type="number" min="0" max="1100" id="inputScore" class="form-control" placeholder="Total points for the Unified State Exam" required>
+            </div>
+            <div class="col-md-6">
+                <label for="inputStatus" class="form-label">Status</label>
+                <select id="inputStatus" class="form-select" required>
+                    <option selected disabled value="">Choose...</option>
+                    <option>Resident</option>
+                    <option>Nonresident</option>
+                </select>
+            </div>
+            <div class="col-md-6">
+                <label for="inputEmail" class="form-label">Email</label>
+                <input type="email" id="inputEmail" class="form-control" placeholder="example@gmail.com" required>
             </div>
             <div class="col-md-12">
                 <button type="submit" class="registration-btn btn btn-dark px-4 mt-2">Add yourself</button>
