927ad281d14a6346da239a10721e8d3b6b0d2d03
theifel <condecrom@gmail.com>
2021-02-26 17:12:02 +0300

    Added an html method in the StringUtil class

--- App/Components/Utils/StringUtil.php
+++ App/Components/Utils/StringUtil.php
@@ -14,4 +14,14 @@ class StringUtil
     {
         return bin2hex(random_bytes($length));
     }
+
+    /**
+     * Returns a string that is safe for html
+     * @param string $string
+     * @return string
+     */
+    public static function html(string $string): string
+    {
+        return htmlspecialchars($string, ENT_QUOTES);
+    }
 }
