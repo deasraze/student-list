3298437741a458fd5b4ef0d68250c1ac29ae73b9
theifel <condecrom@gmail.com>
2021-03-01 21:18:08 +0300

    Updated routes

--- App/config/routes.php
+++ App/config/routes.php
@@ -1,8 +1,11 @@
 <?php
 return [
     '' => 'site/actionIndex',
-    '\\?key=(id|name|surname|sgroup|score)&sort=(asc|desc)' => 'site/actionIndex',
+    '\\?page=[0-9]+' => 'site/actionIndex',
+    '\\?key=(id|name|surname|sgroup|score)&sort=(asc|desc)(&page=[0-9]+)?' => 'site/actionIndex',
     '\\?notification=(added|edited)&for=[0-9a-z]{64}' => 'site/actionIndex',
+
     'form' => 'site/actionForm',
+
     'search\\?search=[\\w+%]+(&key=(id|name|surname|sgroup|score)&sort=(asc|desc))?' => 'site/actionSearch',
 ];
