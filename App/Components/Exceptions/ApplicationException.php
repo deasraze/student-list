3af1dd57e50539afecc018d9eb9eec180db739d0
theifel <condecrom@gmail.com>
2021-03-04 10:23:52 +0300

    Added AplicationException class

---
+++ App/Components/Exceptions/ApplicationException.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Components\Exceptions;
+
+class ApplicationException extends \Exception
+{
+    /**
+     * Status code for response
+     * @var int
+     */
+    private int $statusCode;
+
+    /**
+     * Error description
+     * @var string
+     */
+    private string $description;
+
+    /**
+     * ApplicationException constructor.
+     * @param string $message
+     * @param int $statusCode
+     * @param string $description
+     */
+    public function __construct(
+        string $message,
+        int $statusCode = 503,
+        string $description = 'The server with the site is currently unavailable, please try again later or contact server administrator'
+    ) {
+        parent::__construct($message);
+
+        $this->statusCode = $statusCode;
+        $this->description = $description;
+    }
+
+    /**
+     * Getting status code
+     * @return int
+     */
+    public function getStatusCode(): int
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * Getting error description
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+}
