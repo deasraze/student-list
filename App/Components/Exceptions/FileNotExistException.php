d34f897f099022d3714118d451e1b6514b2b3e53
theifel <condecrom@gmail.com>
2021-03-04 12:09:41 +0300

    Added FileNotExistException

---
+++ App/Components/Exceptions/FileNotExistException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Components\Exceptions;
+
+class FileNotExistException extends ApplicationException
+{
+    /**
+     * FileNotExistException constructor.
+     * @param string $fileName
+     */
+    public function __construct(string $fileName)
+    {
+        parent::__construct("File $fileName no exist. "
+        . "Check whether the file name is correct or create it");
+    }
+}
