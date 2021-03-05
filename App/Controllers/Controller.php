a840feee89237c8fbbace975fff0ab5522e0779d
theifel <condecrom@gmail.com>
2021-03-05 11:49:25 +0300

    Fixed the Controller. Now it returns a Response

--- App/Controllers/Controller.php
+++ App/Controllers/Controller.php
@@ -5,40 +5,43 @@ namespace App\Controllers;
 use App\Components\DIContainer;
 use App\Components\Exceptions\ContainerException;
 use App\Components\Exceptions\FileNotExistException;
+use App\Components\Response;
 
 abstract class Controller
 {
-    /**
-     * @var FrontController
-     */
-    public FrontController $fc;
-
     /**
      * @var DIContainer
      */
     public DIContainer $container;
 
     /**
+     * @var Response
+     */
+    public Response $response;
+
+    /**
      * Controller constructor.
      * @param DIContainer $container
+     * @param Response $response
      */
-    public function __construct(DIContainer $container)
+    public function __construct(DIContainer $container, Response $response)
     {
-        $this->fc = FrontController::getInstance();
         $this->container = $container;
+        $this->response = $response;
     }
 
     /**
-     * Filling in the view and returning the result to the FrontController
+     * Filling in the view and returning the result to the App
      * @param string $template
      * @param array $args
+     * @return Response
      * @throws ContainerException
      * @throws FileNotExistException in render()
      */
-    public function show(string $template, array $args): void
+    public function show(string $template, array $args): Response
     {
         $body = $this->container->get('view')->render($template, $args);
-        $this->fc->setBody($body);
+        return $this->response->withBody($body);
     }
 
 }
