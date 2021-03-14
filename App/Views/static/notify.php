d936b7a18e1fb4046ee477cb1de76cf345a35597
theifel <condecrom@gmail.com>
2021-03-14 19:32:32 +0300

    Added the notify template

---
+++ App/Views/static/notify.php
@@ -0,0 +1,7 @@
+<div class="alert alert-{alert-type} alert-dismissible fade show" role="alert">
+    <h4 class="alert-heading">{alert-heading}</h4>
+    {alert-description}
+    <button type="button" id="notification-close" class="btn-close"
+            data-bs-dismiss="alert" aria-label="Close">
+    </button>
+</div>
